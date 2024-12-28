2c4055873f64409b3cb9d8441252f97e02e7df1b
daniel <daniel.rodinger@protonmail.com>
2024-12-28 19:18:33 +0100

    add initial input files and update main for argument parsing

--- main.cpp
+++ main.cpp
@@ -1,10 +1,41 @@
 #include <iostream>
-#include <sstream>
+#include <vector>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
 #include "Timing.h"
 
+using namespace std;
 
-int main() {
+int NUM_ARGS = 7;
+
+
+void parseArguments(int argc, char** argv, string& InputFile, string& OutputFile, int& generations, bool& measure) {
+    // Check if the number of arguments is correct
+    if (argc < NUM_ARGS) {
+        cerr << "Error: Insufficient arguments.\nUsage: --load <file> --save <file> --generations <n> [--measure]\n";
+        exit(EXIT_FAILURE);
+    }
+
+    for (int i=1; i<argc; i++) {
+        string arg = argv[i];
+        cout << "Argument " << i << ": " << arg << endl;
+    }
+
+}
+
+int main(int argc, char** argv) {
+
+    string InputFile, OutputFile;
+    int generations;
+    bool measure = false;
+
+    parseArguments(argc, argv, InputFile, OutputFile, generations, measure);
+
     
+
+    
+
 
 
 
